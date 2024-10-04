bbc56dd9bd4c2582390dd72423ac803b8cef2c25
Nikesh Sitaula <sitaulanike@gmail.com>
2024-10-04 11:30:53 -0400

    Looping excercises added

---
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) throws InterruptedException {
+
+        int i;
+        for (i = 10; i > 0 ; i--){
+            Thread.sleep(1000);
+            System.out.println(i);
+        }
+        System.out.println("Launch!");
+    }
+}
